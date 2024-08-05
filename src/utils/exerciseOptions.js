a9d9c6c52f6bcf5a28dc8ec2a5f60e894418fe33
DEV BHAWAL <imdevbhawal@gmail.com>
2024-08-05 10:20:27 +0530

    first commit

---
+++ src/utils/exerciseOptions.js
@@ -0,0 +1,7 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.EACT_APP_RAPID_API_KEY,
+    },
+};
